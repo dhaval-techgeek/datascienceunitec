fbfadb8916c9d310bb744af1e11080df7566e082
Dhaval Shah <dhavalce1989@gmail.com>
2020-04-26 12:54:55 +1200

    Lab Exercises 3

--- LabExercises_3/plot3.R
+++ LabExercises_3/plot3.R
@@ -3,7 +3,7 @@ powerConsumption <- read.csv("DataSet.txt",sep = ";")
 png(file='plot3.png')
 
 with(subset(powerConsumption, Voltage>=249), plot(Voltage, Global_active_power))
-with(subset(powerConsumption, Voltage>=249 && Voltage<249), points(Voltage, Global_active_power))
+with(subset(powerConsumption, Voltage>=249 && Voltage<250), points(Voltage, Global_active_power), col = "black")
 with(subset(powerConsumption, Voltage>=250), points(Voltage, Global_active_power, col = "red"))
 title(main="Global Active Power (kilowatts) for Voltage >= 249")
 legend("topright", pch=1, col=c("black","red"), legend=c("Voltage >= 249 & < 250","Voltage >= 250"))
